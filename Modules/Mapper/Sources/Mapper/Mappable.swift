c05c96b1d0725ed1313787ea7709f41c55addd64
Oleg Dreyman <dreymonde@me.com>
2016-09-20 08:59:05 +0300

    Remove excess code

--- Modules/Mapper/Sources/Mapper/Mappable.swift
+++ Modules/Mapper/Sources/Mapper/Mappable.swift
@@ -4,15 +4,3 @@ public typealias Mappable = InMappable & OutMappable
 public protocol StringInMappable: InMappable {
     init<Map: InMap>(mapper: StringInMapper<Map>) throws
 }
-
-public struct Testi: InMappable {
-    let stringi: String
-    public init<Map : InMap>(mapper: InMapper<Map, Keys>) throws {
-        self.stringi = try mapper.map(from: .stringi)
-    }
-    public enum Keys: String, IndexPathElement {
-        case stringi
-    }
-}
-
-
