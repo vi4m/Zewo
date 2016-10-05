b26026167b1eb99eff5a068db7fd2da43980d836
Dan Appel <dan.appel00@gmail.com>
2016-10-05 13:28:14 -0700

    git subrepo pull Modules/SQL
    subrepo:
      subdir:   "Modules/SQL"
      merged:   "360d9e5"
    upstream:
      origin:   "https://github.com/Zewo/SQL"
      branch:   "master"
      commit:   "360d9e5"
    git-subrepo:
      version:  "0.3.0"
      origin:   "https://github.com/ingydotnet/git-subrepo"
      commit:   "1e79595"

--- Modules/SQL/Tests/SQLTests/SQLTests.swift
+++ Modules/SQL/Tests/SQLTests/SQLTests.swift
@@ -1,9 +1,6 @@
 import XCTest
 @testable import SQL
 
-
-import SQL
-
 struct User {
     let username: String
     let password: String
@@ -11,23 +8,23 @@ struct User {
 
 extension User: ModelProtocol {
     typealias PrimaryKey = Int
-    
+
     enum Field: String, ModelField {
         case username
         case password
         case id
-        
+
         static let tableName: String = "users"
         static let primaryKey: Field = .id
     }
-    
+
     func serialize() -> [Field : ValueConvertible?] {
         return [
             .username: username,
             .password: password
         ]
     }
-    
+
     init<Row: RowProtocol>(row: TableRow<User, Row>) throws {
         username = try row.value(.username)
         password = try row.value(.password)
